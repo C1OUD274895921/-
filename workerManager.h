6559808d63bedf1879aa8c8a4df66f0c95c6c82c
C1OUD274895921 <50456272+C1OUD274895921@users.noreply.github.com>
2019-08-22 00:56:33 +0800

    AAAA
    aa

---
+++ workerManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class workerManager
+{
+public:
+	//构造函数
+	workerManager();
+	//析构函数
+	~workerManager();
+	
+	//菜单展示
+	void show_menu();
+	//退出管理系统
+	void exit_system():
+};
+
